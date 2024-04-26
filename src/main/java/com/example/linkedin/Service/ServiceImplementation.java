b85b339ea7b0d5a6c234f4519d791a26026a7815
Yogeeswaran03 <yogees97@gmail.com>
2024-04-26 10:15:45 +0530

    summa podren bro pathukunga

--- src/main/java/com/example/linkedin/Service/ServiceImplementation.java
+++ src/main/java/com/example/linkedin/Service/ServiceImplementation.java
@@ -5,6 +5,7 @@ import com.example.linkedin.Repository.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Objects;
 
 @org.springframework.stereotype.Service
 public class ServiceImplementation implements Service{
@@ -18,6 +19,57 @@ public class ServiceImplementation implements Service{
     @Override
     public List<Profile> getDetails() {
         return (List<Profile>)repository.findAll();
+    }
+
+    @Override
+    public Profile update(Profile profile, Long userid) {
+        Profile prodb=repository.findById(userid).get();
+        if (Objects.nonNull(profile.getUsername())
+                && !"".equalsIgnoreCase(
+                profile.getUsername())) {
+            prodb.setUsername(
+                    profile.getUsername());
+        }
+
+        if (Objects.nonNull(
+                profile.getEmail())
+                && !"".equalsIgnoreCase(
+                profile.getEmail())) {
+            prodb.setEmail(
+                    prodb.getEmail());
+        }
+
+        if (Objects.nonNull(profile.getPassword()) && !"".equalsIgnoreCase(profile.getPassword())) {
+            prodb.setPassword(
+                    profile.getPassword());
+        }
+        if (Objects.nonNull(profile.getJob()) && !"".equalsIgnoreCase(profile.getJob())) {
+            prodb.setJob(
+                    profile.getJob());
+        }
+        if (Objects.nonNull(profile.getBio()) && !"".equalsIgnoreCase(profile.getBio())) {
+            prodb.setBio(
+                    profile.getBio());
+        }
+        if (Objects.nonNull(profile.getQualification()) && !"".equalsIgnoreCase(profile.getQualification())) {
+            prodb.setQualification(
+                    profile.getQualification());
+        }
+        if (Objects.nonNull(profile.getPhoneno())) {
+            prodb.setPhoneno(
+                    profile.getPhoneno());
+        }
+        if (Objects.nonNull(profile.getSkill()) && !"".equalsIgnoreCase(profile.getSkill())) {
+            prodb.setSkill(
+                    profile.getSkill());
+        }
+
+        if (Objects.nonNull(profile.getPhoto()) && !"".equalsIgnoreCase(profile.getPhoto())) {
+            prodb.setPhoto(
+                    profile.getPhoto());
+        }
+
+        return repository.save(prodb);
     }
 
 
